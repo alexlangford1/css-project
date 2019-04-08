ddfe405faf415c5c35b7f4144df52f5e0e0842e5
alex langford <alejandro010795@gmail.com>
2019-04-08 17:47:01 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            menu: false,
+        }
+    }
+
+    dropDown = () => {
+        this.setState({ menu: !this.state.menu })
+    }
+    close = () => {
+        if (this.state.menu) {
+            this.setState({ menu: false })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="App">
+                    <div
+                        className={
+                            this.state.menu ? "drop-down down" : "drop-down"
+                        }
+                    >
+                        <span>SERVICES</span>
+                        <span>PORTFOLIO</span>
+                        <span>ABOUT</span>
+                        <span>TEAM</span>
+                        <span>CONTACT</span>
+                    </div>
+                    <div onClick={this.close}>
+                        <header className="header">
+                            <h1 className="title">Start BootStrap</h1>
+                            <div className="about">
+                                <span>SERVICES</span>
+                                <span>PORTFOLIO</span>
+                                <span>ABOUT</span>
+                                <span>TEAM</span>
+                                <span>CONTACT</span>
+                            </div>
+                            <div onClick={this.dropDown} className="bars">
+                                <div>Menu</div>
+                                <i className="fas fa-bars" />
+                            </div>
+                        </header>
+                        <div className="greeting">
+                            <h2 className="size">Welcome To Our Studio!</h2>
+                            <h1 className="size2">IT'S NICE TO MEET YOU</h1>
+                            <button className="tell-more">TELL ME MORE</button>
+                        </div>
+                        <div className="page2">
+                            <h1>SERVICES</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
